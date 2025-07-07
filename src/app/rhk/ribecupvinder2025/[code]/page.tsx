19f31014460f61ad3ccbabd6b4b299027ed71a1c
Sebastian Hjarnø <sebastian@b2bpromotion.dk>
2025-07-07 12:54:42 +0200

    Updated winner portal

--- src/app/rhk/ribecupvinder2025/[code]/page.tsx
+++ src/app/rhk/ribecupvinder2025/[code]/page.tsx
@@ -1,12 +1,7 @@
-import React, { Suspense } from 'react';
 import ClientWinnerPage from '../ClientWinnerPage';
 
 export const dynamic = 'force-dynamic';
 
-export default async function Page({ params }: { params: { code: string } }) {
-  return (
-    <Suspense fallback={<p className="text-center mt-16">Loading...</p>}>
-      <ClientWinnerPage code={params.code} />
-    </Suspense>
-  );
+export default function Page({ params }: { params: { code: string } }) {
+  return <ClientWinnerPage code={params.code} />;
 }
